ed3220a2aa55cacd4b63d9e690dfec59e485aac3
Benjamin Klaus <klaus@technikum-wien.at>
2022-09-21 16:30:50 +0200

    added start script

---
+++ start.sh
@@ -0,0 +1,22 @@
+# for autostart:
+# sudo nano /etc/xdg/autostart/display.desktop
+# insert:
+# [Desktop Entry]
+# Name=RhasspyStart
+# Exec=/usr/bin/bash /home/pi/rhasspy-bridge/start.sh
+
+# don't do this because it kills this script
+# echo "closing everything..."
+# pkill -15 -f rhasspy
+# pkill -15 python3
+
+echo "start rhasspy..."
+rhasspy -p de > /home/pi/log_rhasspy.txt &
+echo "start bridge..."
+cd /home/pi/rhasspy-bridge/src/ && python3 server.py > /home/pi/log_bridge.txt &
+
+sleep 2
+echo "start browser..."
+#sensible-browser http://localhost:12101/ &
+#sleep 5
+sensible-browser http://localhost:1234/ &
